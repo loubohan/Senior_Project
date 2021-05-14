f47f7964907c71f401560312d47e89415de15180
Bohan Lou <bohanlou@Bohans-MacBook-Pro.local>
2021-05-14 04:35:57 -0400

    upload

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require('firebase-functions');
+const app = require('express')();
+var cors = require('cors');
+// const cors = require('cors')({ origin: true });
+const auth = require('./util/auth');
+
+app.use(cors());
+ 
+const {
+    getAllTodos,
+    getOneTodo,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./APIs/todos')
+
+const { 
+    loginUser,
+    signUpUser,
+    uploadProfilePhoto,
+    getUserDetail,
+    updateUserDetails
+} = require('./APIs/users')
+
+// Todos
+app.get('/todos', auth, getAllTodos);
+app.get('/todo/:todoId', auth, getOneTodo);
+app.post('/todo',auth, postOneTodo);
+app.delete('/todo/:todoId',auth, deleteTodo);
+app.put('/todo/:todoId',auth, editTodo);
+
+// Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
+app.post('/user/image', auth ,uploadProfilePhoto);
+app.post('/user', auth ,updateUserDetails);
+app.get('/user', auth, getUserDetail);
+
+exports.api = functions.https.onRequest(app);
